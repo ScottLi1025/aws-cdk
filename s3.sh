f9cf02bf61639f1df78912feeba67e4bba1ec019
KaiJor <fatjorqaq@gmail.com>
2019-12-11 21:06:35 +0800

    S3 shell script for  upload json file to S3

---
+++ s3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+npm run cdk synth -- -o cdk.out
+
+aws s3 cp "cdk.out/cdnstack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/cfnstack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/cloud9inlinestack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/cognitostack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/dbstack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/iamstack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/parameterstack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/vpcstack.template.json" s3://cfn-cdk
+aws s3 cp "cdk.out/cfnstack.template.json" s3://cfn-cdk
